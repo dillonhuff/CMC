4e28a1530b020c89c552409c47f502998a100b06
Dillon Huff <dillonhuff@gmail.com>
2014-04-25 15:22:20 -0700

    added skeletal body to asm

--- src/Asmx86.hs
+++ src/Asmx86.hs
@@ -2,14 +2,33 @@ module Asmx86(
 	toAsm) where
 
 toAsm :: String -> String
-toAsm name = (prologue name) ++ epilogue
+toAsm name = (prologue name) ++ body ++ epilogue
 
 prologue funcName = (fileDec funcName) ++ textSeg ++ (globl undFunc) ++ (def undFunc) ++ (label undFunc)
 	where
 		undFunc = "_" ++ funcName
 
+body = bodyStart ++ bodyEnd
+
+bodyStart = (pushl "%ebp") ++ (movl "%esp" "%ebp")
+
+bodyEnd = leave ++ ret
+
 epilogue = (def "_malloc") ++ (def "_free")
 
+
+-- Helper functions for instruction generation
+label name = ln (name ++ ":")
+
+pushl reg = tbln ("pushl " ++ reg)
+
+movl dest src = tbln ("movl " ++ dest ++ ", " ++ src)
+
+leave = tbln "leave"
+
+ret = tbln "ret"
+
+-- Helper functions for file declarations
 fileDec name = tbln (".file\t" ++ "\"" ++ name ++ ".cmc\"")
 
 textSeg = tbln ".text"
@@ -18,8 +37,7 @@ globl name = ln (".globl " ++ name)
 
 def name = tbln (".def\t" ++ name ++ ";\t.scl\t2;\t.type 32; .endef")
 
-label name = ln (name ++ ":")
-
+-- General string format helpers
 tbln str = "\t" ++ str ++ "\n"
 
 ln str = str ++ "\n"

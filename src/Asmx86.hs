14e4ebea2aeb70e56539f2d3f9d42ff44c5c74a3
Dillon Huff <dillonhuff@gmail.com>
2014-04-25 15:31:10 -0700

    made some more convenient helpers for asm generation

--- src/Asmx86.hs
+++ src/Asmx86.hs
@@ -10,23 +10,30 @@ prologue funcName = (fileDec funcName) ++ textSeg ++ (globl undFunc) ++ (def und
 
 body = bodyStart ++ bodyEnd
 
-bodyStart = (pushl "%ebp") ++ (movl "%esp" "%ebp")
+bodyStart = instrs [pushl ebp, movl esp ebp]
 
 bodyEnd = leave ++ ret
 
 epilogue = (def "_malloc") ++ (def "_free")
 
+-- Register names
+ebp = "%ebp"
+esp = "%esp"
 
 -- Helper functions for instruction generation
 label name = ln (name ++ ":")
 
-pushl reg = tbln ("pushl " ++ reg)
+pushl reg = uIstr "pushl" reg
 
-movl dest src = tbln ("movl " ++ dest ++ ", " ++ src)
+movl dest src = bInstr "movl" dest src
 
 leave = tbln "leave"
 
 ret = tbln "ret"
+
+uIstr name arg = name ++ " " ++ arg
+
+bInstr name arg1 arg2 = name ++ " " ++ arg1 ++ ", " ++ arg2
 
 -- Helper functions for file declarations
 fileDec name = tbln (".file\t" ++ "\"" ++ name ++ ".cmc\"")
@@ -38,6 +45,8 @@ globl name = ln (".globl " ++ name)
 def name = tbln (".def\t" ++ name ++ ";\t.scl\t2;\t.type 32; .endef")
 
 -- General string format helpers
+instrs iList = concat $ map tbln iList
+
 tbln str = "\t" ++ str ++ "\n"
 
 ln str = str ++ "\n"
